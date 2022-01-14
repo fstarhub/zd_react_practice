b21917286a2b5beee555fbab4a343f997b136cd6
star <star@qq.com>
2022-01-14 16:53:53 +0800

    userApi

---
+++ react-router_V5/src/api/user.js
@@ -0,0 +1,31 @@
+import request from '../utils/request'
+
+export default class UserApi {
+  static login(data) {
+    return request({
+      url: '/users/login',
+      method: 'post',
+      data: data,
+    })
+  }
+
+  static register(data) {
+    return request({
+      url: '/users/register',
+      method: 'post',
+      data: data
+    })
+  }
+
+  static modifyPassword(data) {
+    return request({
+      url: '/users/',
+      method: 'patch',
+      data: data
+    })
+  }
+
+  static test() {
+    console.log('ceshi')
+  }
+}
